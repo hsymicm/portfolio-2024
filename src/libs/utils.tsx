e6cce22beef577584fdcf62b9e54a6f5521a1f9b
hsymicm <ichorvfx@gmail.com>
2024-02-02 23:16:19 +0700

    Seperate CN function to standalone lib

--- src/libs/utils.tsx
+++ src/libs/utils.tsx
@@ -1,10 +1,4 @@
-import { clsx } from "clsx";
 import { useState, useLayoutEffect } from "react";
-import { twMerge } from "tailwind-merge";
-
-export function cn(...args: any[]) {
-  return twMerge(clsx(args));
-}
 
 export function generateRandomNumber(num: number) {
   return Array.from({ length: num }, () => Math.random() * 360);
