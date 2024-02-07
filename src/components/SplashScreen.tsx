824bd8e98feb69be5e14fafacdb3a29b9c4ec3f3
hsymicm <ichorvfx@gmail.com>
2024-02-07 16:17:19 +0700

    update splashscreen and delete animate.tsx

--- src/components/SplashScreen.tsx
+++ src/components/SplashScreen.tsx
@@ -1,34 +1,25 @@
 "use client"
 
-import { FC, useState, useEffect, useRef } from "react"
+import { FC, useState, useEffect } from "react"
 import { cn } from "@/libs/cn"
 import gsap from "gsap"
 
 const SplashScreen: FC = () => {
   const [show, setShow] = useState(true)
 
-  const openDuration = 3
+  const openDuration = 2
 
   useEffect(() => {
-    const body = document.querySelector("body")
-    body.classList.add("loading")
-
     const ctx = gsap.context(() => {
       gsap.to("#bar", {
         onStart: () => {
-          gsap.to("#bg-blocker", {
-            backgroundColor: "transparent",
-            duration: 0,
-            repeat: 0,
-            ease: "none",
-          })
           gsap.to("#stagger-text", {
             yPercent: 100,
             opacity: 0,
             duration: openDuration/3,
-            delay: 0.2,
-            ease: "power3.inOut",
-            stagger: 0.1,
+            delay: openDuration/15,
+            ease: "power4.inOut",
+            stagger: openDuration/30,
             repeat: 0,
           })
           gsap.to("#splash-screen-glow", {
@@ -40,22 +31,19 @@ const SplashScreen: FC = () => {
         },
         yPercent: 100,
         duration: openDuration,
-        ease: "power2.inOut",
-        stagger: 0.3,
+        ease: "power3.inOut",
+        stagger: openDuration/10,
         repeat: 0,
       })
 
       gsap.to("#bar-child", {
         yPercent: 100,
         duration: openDuration * 1.2,
-        delay: 0.1,
-        ease: "power2.inOut",
-        stagger: 0.3,
+        delay: openDuration/30,
+        ease: "power3.inOut",
+        stagger: openDuration/10,
         repeat: 0,
-        onComplete: () => {
-          setShow(false)
-          body.classList.remove("loading")
-        },
+        onComplete: () => setShow(false),
       })
     })
 
@@ -90,10 +78,6 @@ const SplashScreen: FC = () => {
         <span id="stagger-text" className="">.</span>
       </div>
       <div id="splash-screen-glow" className="absolute left-1/2 -translate-x-1/2 -bottom-[380px] w-[540px] bg-white/40 aspect-square rounded-full blur-[300px] z-10" />
-      <div
-        id="bg-blocker"
-        className="absolute top-0 left-0 bg-black w-screen h-screen"
-      ></div>
       <div className="absolute top-0 left-0 flex  w-screen h-screen">
         <div id="bar" className="w-1/5 bg-black border-2 border-black"></div>
         <div id="bar" className="w-1/5 bg-black border-2 border-black"></div>
